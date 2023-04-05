773f2f672322d033a8e08fee3d331b76ef780f2a
Ashraful Haque <vselfnet@gmail.com>
2023-04-05 14:49:35 +0200

    Cart Info Shared Two Pages Shop & Orders

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -5,19 +5,24 @@ import '../Cart/Cart.css';
 import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
-    const products = useLoaderData();
+    // cart shared only one page [Shop]
+    // const products = useLoaderData();
     // console.log(products)
+
+    // this cart share both page [Shop & Orders] from cartProductsLoader.js
+    const cart = useLoaderData();
+    
     return (
         <div className='shop-container'>
             
             <div className='products'>
-                <h2>Order: {products.length}</h2>
-                <h2>Order:</h2>
+                {/* <h2>Order: {products.length}</h2> */}
+                <h2>Order: {cart.length}</h2>
             </div>
 
             <div className='cart'>
                 
-                <Cart cart={[]}> </Cart>
+                <Cart cart={cart}> </Cart>
             </div>
 
         </div>
--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,8 +1,35 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductsLoader = async () => {
+    // step-1
     const loadedProducts = await fetch('products.json');
     const products = await loadedProducts.json();
-    console.log(products);
-    return products;
+
+    // step-2
+    // must use async, ehen cart is in DB
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+
+    // we get select item length from local storage
+    // console.log(products);
+    for(const id in storedCart){
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct)
+        }
+    }
+    
+    // can return two value like this, if needed
+    // return [products, savedCart];
+    // return {products, savedCart}
+    // return {products, cart: savedCart}
+    
+    return savedCart;
+
+    // step-1
+    // return products;
 }
 
 export default cartProductsLoader;
